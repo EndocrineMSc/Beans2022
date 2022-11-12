dd75bf3e0d4caf98933ba2301e093fb1838bb59b
JonasGruetzmacher <61141274+JonasGruetzmacher@users.noreply.github.com>
2022-11-12 19:07:22 +0100

    blinking

--- Assets/CameraManager.cs
+++ Assets/CameraManager.cs
@@ -8,6 +8,10 @@ public class CameraManager : MonoBehaviour
 
     [SerializeField] CinemachineRecomposer cineRecomp;
     [SerializeField] CinemachineStoryboard cineSB;
+    public float blinkSpeed;
+    public float closedTime;
+
+    bool isBlinking = false;
 
     float startSpeed;
     // Start is called before the first frame update
@@ -19,8 +23,61 @@ public class CameraManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //currentSpeed = GameManager.Instance.Speed;
         cineRecomp.m_ZoomScale = 1 + (startSpeed - GameManager.Instance.Speed) * 0.1f;
+        if(Input.GetKeyDown(KeyCode.B) && !isBlinking)
+        {
+            Blink();
+        }
+    }
+    void Blink()
+    {
+        isBlinking = true;
+        StartCoroutine(blinkIn());
+    }
 
+    IEnumerator blinkIn()
+    {
+        
+        while (cineSB.m_Alpha < 1)
+        {
+            cineSB.m_Alpha += 0.02f;
+            cineSB.m_Scale.y -= 0.01f;
+            yield return new WaitForSeconds(blinkSpeed * 0.02f);
+        }
+        StartCoroutine (waitBlinkClosing());
+    }
+    IEnumerator waitBlinkClosing()
+    {
+        int i = 0;
+        while (i < closedTime * 50)
+        {
+            i++;
+            cineSB.m_Scale.y -= 0.005f;
+            yield return new WaitForSeconds(0.01f);
+        }
+        StartCoroutine(waitBlinkOpen());
+    }
+    IEnumerator waitBlinkOpen()
+    {
+        int i = 0;
+        while (i < closedTime * 50)
+        {
+            i++;
+            cineSB.m_Scale.y += 0.005f;
+            yield return new WaitForSeconds(0.01f);
+        }
+        StartCoroutine(blinkOut());
+    }
+    IEnumerator blinkOut()
+    {
+        while (cineSB.m_Alpha > 0)
+        {
+            cineSB.m_Alpha -= 0.02f;
+            cineSB.m_Scale.y += 0.01f;
+            yield return new WaitForSeconds(blinkSpeed * 0.02f);
+        }
+        cineSB.m_Alpha = 0;
+        cineSB.m_Scale.y = 2.2f;
+        isBlinking = false;
     }
 }
